c0c3e6bcead94f1bb37980938699a7811834c453
vivi nessa tampubolon <if320058@students.del.ac.id>
2023-05-04 15:56:22 +0700

    Create halaman informasi detail kak seto school

--- resources/views/kaksetoschool.blade.php
+++ resources/views/kaksetoschool.blade.php
@@ -36,8 +36,78 @@
       </ul>
     </div>
     <div class="w-full mt-8  bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
-    <p>Hello</p>
+    <div class="relative overflow-hidden bg-white">
+  <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+    <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+      <div class="sm:max-w-lg">
+        <h1 class="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">SEKOLAH KAK SETO</h1>
+        <p class="mt-4 text-xl text-gray-500">"Belajar dapat dilakukan kapan saja, di mana saja, dan dengan siapa saja"</p>
+      </div>
+      <div>
+        <div class="mt-10">
+          <!-- Decorative image grid -->
+          <div aria-hidden="true" class="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
+            <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8"> 
+                <img class="h-auto max-w-lg ml-auto" src="https://hsks.sch.id/images/hskakseto/features/kakseto_art.png" alt="image description">
+            </div>
+        </div>
+        </div>
+      </div>   
+      </div>
     
+    </div>   
+   
+  </div>
+ 
+<article class="md:gap-8 md:grid md:grid-cols-3">
+    <div class="col-span-2 mt-6 md:mt-0">
+        <div class="flex items-start mb-5">
+            <div class="pr-4">
+                <footer>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">Reviewed: <time datetime="2022-01-20 19:00">January 20, 2022</time></p>
+                </footer>
+                <h4 class="text-xl font-bold text-gray-900 dark:text-white">"Setiap anak mempunyai hak yang sama untuk memperoleh pendidikan yang layak bagi dirinya"</h4>
+            </div>
+        </div>
+        <p class="mb-2 text-gray-500 dark:text-gray-400">Dalam Pengalaman dan pengamatan di lapangan menunjukkan banyak anak yang mendapatkan pengalaman kurang menyenangkan selama bersekolah. Diantaranya adalah kasus kekerasan(bullying) baik secara fisik maupun verbal, dan juga pemasungan terhadap kreativitas anak. Pengalaman - pengalaman tersebut yang menimbulkan ketakutan anak dan orang tua terhadap sekolah (school phobia).</p>
+        <p class="mb-5 text-gray-500 dark:text-gray-400">Juga adanya penyeragaman kemampuan anak di segala bidang mematikan proses berfikir kreatif yang menjadi naluri alami seorang anak. Karena pada dasarnya setiap anak mempunyai keunikannya masing - masing.</p>
+    </div>
+</article>
+
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">Kurikulum Sekolah Kak Seto</h1>
+<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Cerdas, Kreatif, Ceria</p>
+
+<div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2" style="background-color: cadetblue;">
+    <figure class="flex flex-col items-center justify-center p-8 text-center bg-blue border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">TAKSONOMI BLOOM</h3>
+            <p class="my-4">If you care for your time, I hands down would go with this."</p>
+        </blockquote>
+          
+    </figure>
+    <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-tr-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ASPEK PSIKOMOTORIK</h3>
+            <p class="my-4">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"</p>
+        </blockquote>
+           
+    </figure>
+    <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-bl-lg md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ASPEK AFEKTIF</h3>
+            <p class="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>
+        </blockquote>
+          
+    </figure>
+    <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">ASPEK KOGNITIF</h3>
+            <p class="my-4">You have many examples that can be used to create a fast prototype for your team."</p>
+        </blockquote>  
+    </figure>
+</div>
+
+    </div>  
   </div> 
 </div>
 
